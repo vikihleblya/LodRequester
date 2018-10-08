b1e5b76ad73526fad22962aeea19e7560e2fc38d
vikihleblya <vikihleblya@gmail.com>
2018-10-08 15:23:34 +0300

    added hometask

--- app/src/main/java/com/example/roman/lodaddplaction/data/RequestProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.example.roman.lodaddplaction.data
-
-import com.example.roman.lodaddplaction.models.RequestModel
-
-interface RequestProvider {
-    fun getRequests() : List<RequestModel>
-}
--- app/src/main/java/com/example/roman/lodaddplaction/models/UserModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.roman.lodaddplaction.models
-
-import java.io.Serializable
-
-data class UserModel(
-     val name: String,
-     val avaUrl: String
-) : Serializable
